50f69849143edf8f3d430b76c235f5f84b5de1be
seungminkim <ksm8797@naver.com>
2024-03-20 00:11:32 +0900

    feature : add timeOut interceptor

--- apps/rest-api-gateway/src/rest-api-gateway.module.ts
+++ apps/rest-api-gateway/src/rest-api-gateway.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common'
+import { Module, OnApplicationBootstrap } from '@nestjs/common'
 import { UserModule } from '../api/user/user.module'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import configurations from '../../grpc-user/src/config/configurations'
 import { GrpcStubUserModule } from '../../../grpc-stub/src/grpc-stub-user'
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, BaseExceptionFilter, HttpAdapterHost } from '@nestjs/core'
+import { ClsGuard, ClsInterceptor } from 'nestjs-cls'
+import { Server } from 'http'
+import { TimeoutInterceptor } from '../../../libs/interceptor/timeout.interceptor'
 
 @Module({
   imports: [
@@ -23,6 +27,19 @@ import { GrpcStubUserModule } from '../../../grpc-stub/src/grpc-stub-user'
     UserModule,
   ],
   // TODO GUARD, FILTER, APP_INTERCEPTOR 추가 필요
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TimeoutInterceptor,
+    },
+  ],
 })
-export class RestApiGatewayModule {}
+export class RestApiGatewayModule implements OnApplicationBootstrap {
+  constructor(private readonly refHost: HttpAdapterHost<any>) {}
+
+  onApplicationBootstrap() {
+    const server: Server = this.refHost.httpAdapter.getHttpServer()
+    server.keepAliveTimeout = 61 * 1000
+    server.headersTimeout = 65 * 1000
+  }
+}

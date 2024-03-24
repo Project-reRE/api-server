33312e0071f4b650ec2c9dacb42fe32a2a74dc25
seungminkim <ksm8797@naver.com>
2024-03-24 16:45:37 +0900

    feature : add swagger Module

--- apps/rest-api-gateway/src/main.ts
+++ apps/rest-api-gateway/src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { RestApiGatewayModule } from './rest-api-gateway.module';
+import { NestFactory } from '@nestjs/core'
+import { RestApiGatewayModule } from './rest-api-gateway.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(RestApiGatewayModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(RestApiGatewayModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('API Server')
+    .setDescription('The rere API description')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(3000)
 }
-bootstrap();
+
+bootstrap()

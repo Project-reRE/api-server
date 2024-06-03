50e628cb10c893ec1ecaf95958ebb82380f2c76d
seungminkim <ksm8797@naver.com>
2024-06-03 22:21:17 +0900

    feature : add role guard

---
+++ apps/rest-api-gateway/src/guards/roles.guard.ts
@@ -0,0 +1,24 @@
+
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector, private jwtService: JwtService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization?.split(' ')[1];
+    if (!token) {
+      return false;
+    }
+
+    const user = this.jwtService.verify(token);
+    return roles.includes(user.role);
+  }
+}

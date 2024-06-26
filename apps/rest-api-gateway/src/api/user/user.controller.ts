c1ff1db6e9589d17b8fc81f072fe9a375852e2cd
seungminkim <ksm8797@naver.com>
2024-06-26 18:47:34 +0900

    feature : add response body example to user api

--- apps/rest-api-gateway/src/api/user/user.controller.ts
+++ apps/rest-api-gateway/src/api/user/user.controller.ts
@@ -5,7 +5,7 @@ import { CreateUserRequestDto } from './dto/create-user-request.dto.st'
 import { CreateUserResponseDto } from './dto/create-user-response.dto'
 import { plainToInstance } from 'class-transformer'
 import { FindOneUserResponseDto } from './dto/find-one-user-response.dto'
-import { ApiTags } from '@nestjs/swagger'
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
 
 @Controller()
 @ApiTags('users')
@@ -13,6 +13,11 @@ export class UserController {
   constructor(private readonly grpcStubUserService: GrpcStubUserService) {}
 
   @Get('/users/:userId')
+  @ApiOperation({
+    summary: '유저 정보 상세 조회',
+    description: '유저 정보 상세 조회',
+  })
+  @ApiOkResponse({ type: FindOneUserResponseDto })
   async findOne(@Param('userId') userId: string): Promise<FindOneUserResponseDto> {
     console.log({ methodName: 'findOne', data: userId, context: 'userId' })
 
@@ -24,6 +29,14 @@ export class UserController {
   }
 
   @Post('/users')
+  @ApiOperation({
+    summary: '유저 생성',
+    description: '유저 생성',
+  })
+  @ApiCreatedResponse({
+    type: CreateUserResponseDto,
+    description: 'application/json.',
+  })
   async createUser(@Body() request: CreateUserRequestDto): Promise<CreateUserResponseDto> {
     console.log({ methodName: 'createUser', data: request, context: 'request' })
     const { user: createdUser } = await firstValueFrom(this.grpcStubUserService.createUser(request))

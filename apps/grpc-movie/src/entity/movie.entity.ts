4f8f1eae7a3612e356e4645c1c64322db993ddcb
최동선 <64996188+CHOI-DONGSUN@users.noreply.github.com>
2024-07-04 15:25:05 +0900

    Update movie.entity.ts

--- apps/grpc-movie/src/entity/movie.entity.ts
+++ apps/grpc-movie/src/entity/movie.entity.ts
@@ -1,34 +1,34 @@
 import { Column, Entity, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn, PrimaryColumn } from "typeorm";
 import { Transform } from 'class-transformer'
 
-@Entity('movies')
+@Entity('movie')
 export class MovieEntity {
 
     @PrimaryColumn( { type: 'varchar', unique: true, length: 20 })
     id: string;
 
-    @Column({ type: 'varchar', unique: true, length: 45 })
+    @Column({ type: 'varchar', length: 45 })
     provider: string;
 
-    @Column({ type: 'varchar', unique: true, length: 45 })
+    @Column({ type: 'varchar', length: 128 })
     name: string;
 
-    @Column({ type: 'varchar', unique: true, length: 45 })
+    @Column({ type: 'varchar', length: 45 })
     nameOrg: string;
 
-    @Column({ type: 'varchar', unique: true, length: 45 })
+    @Column({ type: 'int'})
     prodYear: number;
 
-    @Column({ type: 'varchar', unique: true, length: 45 })
+    @Column({ type: 'varchar', length: 256 })
     poster: string;
 
-    @Column({ type: 'varchar', unique: true, length: 45 })
+    @Column({ type: 'varchar', length: 128 })
     genre: string;
 
-    @Column({ type: 'varchar', unique: true, length: 45 })
+    @Column({ type: 'varchar', length: 128 })
     director: string;
 
-    @Column({ type: 'varchar', unique: true, length: 45 })
+    @Column({ type: 'varchar', length: 1000 })
     actor: string;
 
     @Column({ type: 'json'})

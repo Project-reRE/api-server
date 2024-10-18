be2aa9f7e0cddecb9562cd1d413d30817843ba0a
yuhyeon Eom <dbgus0101@gmail.com>
2024-10-18 20:07:28 +0900

    chore: git action test (#35)
    Co-authored-by: Eom yuhyeon <eomyuhyeon@Eoms-MacBook-Pro.local>

--- grpc-idl/ts/user.service.ts
+++ grpc-idl/ts/user.service.ts
@@ -1,83 +1,83 @@
 /* eslint-disable */
-import { GrpcMethod, GrpcStreamMethod } from "@nestjs/microservices";
-import { Observable } from "rxjs";
-import { User } from "./user";
+import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices'
+import { Observable } from 'rxjs'
+import { User } from './user'
 
-export const protobufPackage = "user.service";
+export const protobufPackage = 'user.service'
 
 export interface FindOneUserExternalIdRequest {
-  externalId: string;
+  externalId: string
 }
 
 export interface FindOneUserExternalIdResponse {
-  user?: User | undefined;
+  user?: User | undefined
 }
 
 export interface FindOneUserRequest {
-  id: string;
+  id: string
 }
 
 export interface FindOneUserResponse {
-  user?: User | undefined;
+  user?: User | undefined
 }
 
 export interface CreateUserRequest {
-  externalId?: string | undefined;
-  nickName: string;
-  description?: string | undefined;
-  password?: string | undefined;
-  profileUrl?: string | undefined;
-  email?: string | undefined;
-  provider?: string | undefined;
-  role?: string | undefined;
-  gender?: boolean | undefined;
-  birthDate?: string | undefined;
+  externalId?: string | undefined
+  nickName: string
+  description?: string | undefined
+  password?: string | undefined
+  profileUrl?: string | undefined
+  email?: string | undefined
+  provider?: string | undefined
+  role?: string | undefined
+  gender?: boolean | undefined
+  birthDate?: string | undefined
 }
 
 export interface CreateUserResponse {
-  user?: User | undefined;
+  user?: User | undefined
 }
 
-export const USER_SERVICE_PACKAGE_NAME = "user.service";
+export const USER_SERVICE_PACKAGE_NAME = 'user.service'
 
 export interface UserServiceClient {
-  findOneUserExternalId(request: FindOneUserExternalIdRequest, ...rest: any): Observable<FindOneUserExternalIdResponse>;
+  findOneUserExternalId(request: FindOneUserExternalIdRequest, ...rest: any): Observable<FindOneUserExternalIdResponse>
 
-  findOneUser(request: FindOneUserRequest, ...rest: any): Observable<FindOneUserResponse>;
+  findOneUser(request: FindOneUserRequest, ...rest: any): Observable<FindOneUserResponse>
 
-  createUser(request: CreateUserRequest, ...rest: any): Observable<CreateUserResponse>;
+  createUser(request: CreateUserRequest, ...rest: any): Observable<CreateUserResponse>
 }
 
 export interface UserServiceController {
   findOneUserExternalId(
     request: FindOneUserExternalIdRequest,
     ...rest: any
-  ): Promise<FindOneUserExternalIdResponse> | Observable<FindOneUserExternalIdResponse> | FindOneUserExternalIdResponse;
+  ): Promise<FindOneUserExternalIdResponse> | Observable<FindOneUserExternalIdResponse> | FindOneUserExternalIdResponse
 
   findOneUser(
     request: FindOneUserRequest,
     ...rest: any
-  ): Promise<FindOneUserResponse> | Observable<FindOneUserResponse> | FindOneUserResponse;
+  ): Promise<FindOneUserResponse> | Observable<FindOneUserResponse> | FindOneUserResponse
 
   createUser(
     request: CreateUserRequest,
     ...rest: any
-  ): Promise<CreateUserResponse> | Observable<CreateUserResponse> | CreateUserResponse;
+  ): Promise<CreateUserResponse> | Observable<CreateUserResponse> | CreateUserResponse
 }
 
 export function UserServiceControllerMethods() {
   return function (constructor: Function) {
-    const grpcMethods: string[] = ["findOneUserExternalId", "findOneUser", "createUser"];
+    const grpcMethods: string[] = ['findOneUserExternalId', 'findOneUser', 'createUser']
     for (const method of grpcMethods) {
-      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
-      GrpcMethod("UserService", method)(constructor.prototype[method], method, descriptor);
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method)
+      GrpcMethod('UserService', method)(constructor.prototype[method], method, descriptor)
     }
-    const grpcStreamMethods: string[] = [];
+    const grpcStreamMethods: string[] = []
     for (const method of grpcStreamMethods) {
-      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
-      GrpcStreamMethod("UserService", method)(constructor.prototype[method], method, descriptor);
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method)
+      GrpcStreamMethod('UserService', method)(constructor.prototype[method], method, descriptor)
     }
-  };
+  }
 }
 
-export const USER_SERVICE_NAME = "UserService";
+export const USER_SERVICE_NAME = 'UserService'

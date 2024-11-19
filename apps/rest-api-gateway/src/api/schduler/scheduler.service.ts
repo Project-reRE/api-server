0a37ae711dfb303b93e8763330355691327d5532
Eom yuhyeon <eomyuhyeon@Eoms-MacBook-Pro.local>
2024-11-19 21:54:08 +0900

    chore: worker time check

--- apps/rest-api-gateway/src/api/schduler/scheduler.service.ts
+++ apps/rest-api-gateway/src/api/schduler/scheduler.service.ts
@@ -32,6 +32,8 @@ export class SchedulerService {
       코메디: [],
     }
 
+    console.log('worker test', new Date(), '0--------------------')
+
     const today = new Date()
     const year = today.getFullYear()
     const month = String(today.getMonth() + 1).padStart(2, '0') // 월은 0부터 시작하므로 +1 필요
